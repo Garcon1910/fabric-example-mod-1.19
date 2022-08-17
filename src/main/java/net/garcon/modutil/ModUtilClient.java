766522461ff9c9757b6e2947ac9e38f9505b25bb
BuildTools <unconfigured@null.spigotmc.org>
2022-08-17 20:41:50 +0200

    xd

--- src/main/java/net/garcon/modutil/ModUtilClient.java
+++ src/main/java/net/garcon/modutil/ModUtilClient.java
@@ -1,7 +1,7 @@
 package net.garcon.modutil;
 
 import net.fabricmc.api.ClientModInitializer;
-
+// a
 public class ModUtilClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
