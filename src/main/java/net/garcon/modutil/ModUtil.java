195683f2970f8f47bbdfe42a77c6cabc67a4e5df
BuildTools <unconfigured@null.spigotmc.org>
2022-08-17 20:40:44 +0200

    Initial commit

---
+++ src/main/java/net/garcon/modutil/ModUtil.java
@@ -0,0 +1,16 @@
+package net.garcon.modutil;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ModUtil implements ModInitializer {
+
+	public static final String MOD_ID = "modutil";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/garcon/modutil/ModUtilClient.java
@@ -0,0 +1,10 @@
+package net.garcon.modutil;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModUtilClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
